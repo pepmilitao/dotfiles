6a1bd1dbfc6564f3d16c16f761b35838aa1a834d
Pedro Militão <pepmilitao@gmail.com>
2026-03-25 22:49:20 -0300

    Removed neorg, altered treesitter and added diagnostics and start screen

--- apps/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ apps/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,17 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    tag = "v0.10.0",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
     config = function()
-        local configs = require("nvim-treesitter.configs")
+        local configs = require("nvim-treesitter")
 
         configs.setup({
             ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "python", "rust", "bash" },
             sync_install = false,
             highlight = { enable = true },
-            indent = { enable = true },  
+            indent = { enable = true },
         })
     end
 }
